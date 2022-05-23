21595a846566679bf46fd92b693cb3ba588d940a
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-05-22 21:33:23 -0500

    Done

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "John";
+let lastName = "Morgan";
+
+var myAge = "45";
+console.log (firstName);
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templetfullName = `${firstName} ${lastName}`;
+
+console.log("fullName", fullName);
+console.log ("templatefullName",
+ templetfullName);
+
 
 // Exercise 3
+let myStory;
+let city = "Dodge City"
+let myHobbyies = "Bike riding, hiking, flyfishing, spending time with friends and family ";
+
+myStory = `Hello my name is ${fullName} 
+I live in ${city}
+my hobbies are ${myHobbyies}`;
+ console.log(myStory)
+
+myStory = "My name is" +  fullName + " ." + "I live in" + city + " ."
++ "I like" + myHobbyies + " .";
+console.log(myStory)
